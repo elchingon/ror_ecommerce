6e12a3e817a301ff444604d4cc05efc48347611f
Alexi Carey <ciegoproductions@gmail.com>
2014-08-22 02:10:50 -0600

    Add phone_type factory

--- spec/factories/phone.rb
+++ spec/factories/phone.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :phone do
-    phone_type   { PhoneType.first }
+    phone_type   { create(:phone_type) }
     phoneable_type       'User'
     phoneable_id       { FactoryGirl.create(:user).id }
     number          '919-636-0383'
---
+++ spec/factories/phone_types.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :phone_type do
+    name  'Cell'
+  end
+end
